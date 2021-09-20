8b4191b10fd9a89b0588ab2d8089e950344a68e0
taibd1 <ductai.bui@aia.com>
2021-09-20 20:53:41 +0700

    initial commit

---
+++ pages/vietnam.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function VietnamPage() {
+  return (
+    <div>
+      <span class="inline-grid grid-cols-3 gap-x-4">
+        <span>1</span>
+        <span>1</span>
+        <span>1</span>
+      </span>
+      <span class="inline-grid grid-cols-3 gap-x-4">
+        <span>2</span>
+        <span>2</span>
+        <span>2</span>
+      </span>
+    </div>
+  )
+}
